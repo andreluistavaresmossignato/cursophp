f8a55354eea117ea8b314ae565373fdfcea29ead
André Luís Tavares Mossignato <andreluis.mossignato@gmail.com>
2025-03-13 21:21:15 -0300

    Ex000

---
+++ Exercícios/ex000/index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Me livrando da maldição</title>
+</head>
+<body>
+
+    <h1>
+        <?php 
+            echo "Olá, Mundo! \u{1F30E}";
+            // podemos usar também print 
+            // \u{} permite emojis
+        ?>
+    </h1>
+
+    <p>Vamos nos livrar da maldição</p>
+    
+</body>
+</html>
